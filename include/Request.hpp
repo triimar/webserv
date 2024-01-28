751170cef0adbf8e7ff2e9b8938def443d75dd19
Maria Jose Travez <mtravez@student.42heilbronn.de>
2024-01-28 15:37:20 +0100

    Merged ?

--- include/Request.hpp
+++ include/Request.hpp
@@ -2,59 +2,105 @@
 
 #include <map>
 #include <string>
+#include <iostream>
 #include <sstream>
-#include <vector>
+#include <deque>
+#include <cctype>
+#include <cstdlib>
 
-enum RequestMethod { //same enum in Location
-	GET,
-	POST,
-	DELETE,
-	ALL,
-};
+#define CRLF "\r\n"
+#define CRLFCRLF "\r\n\r\n"
 
-enum RequestState {
+enum ParseState {
+	stateParseRequestLine,
+	stateParseHeaders,
+	stateCheckBody,
+	stateParseMessageBody,
+	stateParseChunkedBody,
+	requestParseFAIL, //indicates falure of parser as opposed to problem with request
+	requestERROR, // indicates a problem in the request
 	requestOK,
-	reqestParseFAIL,
-	requestERROR,
 };
 
-//parses the request into needed data, will set stare to requestParseFAIL in case of invalid file & 
-//requesrERROR in case of some other error (*not sure if this is necessary);
-class Request 
+enum RequestLineState {
+	stateParseMethod,
+	stateParseUri,
+	stateParseHTTPver,
+	requestLineOK,
+};
+
+enum RequestMethod {GET, POST, DELETE, OTHER};
+
+// REQUEST CLASS provides functions to parse the request and save the Request data.
+class Request
 {
 private:
-	RequestState	state_;
+	ParseState			state_;
+	RequestLineState	rlstate_;
 
-	RequestMethod	method_;
-	std::string		url_;
-	std::string		httpVer_;
+	std::string			methodStr_;
+	RequestMethod		method_;
+	std::string			uri_; //can be either abs URI or abs path. I dont handle '*' or authority
+	std::string         path_; //is absolute path in relation to the root of the server, extracted from URI
+	std::string			params_;
+	std::string     	query_;
+	std::string         fragment_;
+	std::string			httpVer_;
 
-	// std::map<std::string, std::string> headers_; //map does not allow access using index. To iterate through map I have to make the variable public
-	std::vector<std::pair<std::string, std::string>> headers_;
-	std::vector<char> body_;
-	//std::istringstream	body_; 
-	// or std::vector<char> body_; HAVE to decide, which would be better
-	// Not std::string, because HTTP request can contain binary data. 
-	// In the context of HTTP, "binary data" typically refers to non-textual data, 
-	// which may include images, audio files, video files, or any other type of file that is not represented as plain text.
+	std::map<std::string, std::string> headers_;
 
-	Request();
+
+	int					errorCode_; //is 0 if no error is found in
+	std::string			errorMsg_;
+
 	Request(const Request& rhs);
 	Request &operator=(const Request& rhs);
 
-	std::string& trimWhitespaces(std::string& str);
+	void		parseMethod(std::stringstream& requestLine);
+	void		parseURI(std::stringstream& requestLine);
+	void		parseHTTPver(std::stringstream& requestLine);
+
+	// void 		createHeadersStream(std::stringstream& headersStream, const char *requestBuf, const char *msgEnd);
+	const char *extractHeadersStream(std::stringstream& headersStream, const char *requestBuf, const char *msgEnd);
+	void		parseRequestLine(std::stringstream& headersStream);
+	void		parseHeader(std::stringstream& headersStream);
+	// void		checkForBody(char *requestBuf, char *msgEnd);
+	void		checkForBody(const char *bodyStart, const char *msgEnd);
+
+	void 		setError(ParseState type, int errorCode, const char *message);
+	std::string& trimString(std::string& str);
+	bool 		containsControlChar(std::string& str) const;
+
+
 
 public:
-	Request(char *buffer);
+	Request();
 	~Request();
 
-	void parseRequestLine(char *buffer);
-	
+	// void				processHeaders(const char* requestBuf, int messageLen);
+	void				processRequest(const char* requestBuf, int messageLen);
+	void				storeBody(const char *bodyStart, const char *msgEnd);
+	void 				decodeChunked(const char *bodyStart, const char *msgEnd);
+	void				clearRequest();
 
-	RequestMethod	getMethod() const;
-	std::string		getUrl() const;
-	std::string		getHttpVer()const;
-	std::string		getHeaderKey(int index) const;
-	std::string		getHeaderValue(int index) const;
+	const RequestMethod& getMethod() const;
+	const std::string&	getUri() const;
 
+	const std::string& 	getHttpVer() const;
+	const std::string&	getPath() const;
+	const std::string&	getQuery() const;
+	const std::string&  getFragment() const;
+	bool				isTransferEncodingChunked() const;
+	bool 				isConnectionClose() const;
+	std::map<std::string, std::string>::const_iterator	getHeadersBegin() const;
+	std::map<std::string, std::string>::const_iterator	getHeadersEnd() const;
+	std::string			getHeaderValueForKey(const std::string& key) const;
+
+	const int&			getErrorCode() const;
+	const std::string&	getErrorMsg() const;
+	//can be used to get Content-Type. Will return empty string if its not found in headers
+	std::deque<char> 	body_; //temporarily in public, will go to private with access functions
+	// Not std::string body_, because HTTP request can contain binary data.
+	// In the context of HTTP, "binary data" typically refers to non-textual data,
+	// which may include images, audio files, video files, or any other type of file that is not represented as plain text.
 };
