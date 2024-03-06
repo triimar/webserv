d867028b3995d3b04b3f4e842154650fa946a306
Aaron Rabenstein <arabenst@student.42heilbronn.de>
2024-03-06 14:17:02 +0100

    response headers simpler

--- src/response/construct.cpp
+++ src/response/construct.cpp
@@ -5,17 +5,15 @@ void Response::constructResponse() {
         _body.clear();
     }
     _response.clear();
-    if (_status >= 300) {
-        if (_status >= 400) {
-            makeErrorPage();
-        }
-        _headers["content-length"] = SSTR(_body.size());
+    if (_status >= 400) {
+        makeErrorPage();
     }
     appendStringToVector(_response, HTTP_VERSION);
     appendStringToVector(_response, " ");
     appendStringToVector(_response, Server::getStatusMessage(_status));
     appendStringToVector(_response, CRLF);
     _headers["server"] = _server.getServerName();
+    _headers["content-length"] = SSTR(_body.size());
     _headers["date"] = formatDate(time(0));
     for (std::map<std::string, std::string>::iterator it = _headers.begin();
         it != _headers.end(); ++it) {
@@ -30,18 +28,12 @@ void Response::constructResponse() {
         _response.insert(_response.end(), _body.begin(), _body.end());
         appendStringToVector(_response, CRLFCRLF);
     }
-
-    std::cout << "RESPONSE HEADERS:" << std::endl;
-    for (std::map<std::string, std::string>::iterator it = _headers.begin();
-        it != _headers.end(); ++it) {
-        std::cout << it->first << ": " << it->second << std::endl;
-    }
 }
 
 void Response::makeErrorPage() {
+    _body.clear();
     std::string path = _server.getErrorPage(_status);
     if (path.empty() == false) {
-        _body.clear();
         int fd = open(path.c_str(), O_RDONLY);
         if (readToVector(fd, _body) == RETURN_FAILURE) {
             appendStringToVector(_body, Server::getStatusMessage(_status));
